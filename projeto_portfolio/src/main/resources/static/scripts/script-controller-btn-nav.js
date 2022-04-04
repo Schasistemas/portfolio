ab26f9e8b7d807854d6d40a3df5cfc0bd93dbdf6
Schasistemas <schasistemas@gmail.com>
2022-04-04 18:14:22 -0300

    Atualização de front portfólios

--- projeto_portfolio/src/main/resources/static/scripts/script-controller-btn-nav.js
+++ projeto_portfolio/src/main/resources/static/scripts/script-controller-btn-nav.js
@@ -2,13 +2,28 @@ var BTN_NAV = new Object();
 
 $(document).ready(function () {
      
+     window.scrollTo(0, 0);
+     $(".amb-section").load("layouts/dachboard.html");
+     
      BTN_NAV.buscarLayout = function (index) {
           if (index == 1) {
                $(".amb-section").load("layouts/dachboard.html")
+               window.scrollTo(0, 0);
           } else if (index == 2) {
                $(".amb-section").load("layouts/candidatos.html")
+               window.scrollTo(0, 0);
           } else if (index == 3) {
                $(".amb-section").load("layouts/vagas.html")
+               window.scrollTo(0, 0);
+          } else if (index == 4) {
+               $(".amb-section").load("layouts/clientes.html")
+               window.scrollTo(0, 0);
+          } else if (index == 6) {
+               $(".amb-section").load("layouts/acessos.html")
+               window.scrollTo(0, 0);
+          } else if (index == 0) {
+               $(".amb-section").load("layouts/dachboard.html")
+               window.scrollTo(0, 0);
           }
      }
 
